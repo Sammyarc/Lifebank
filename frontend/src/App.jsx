bfdf2a74619b65473c24edb200a0e907ebeac41f
Sammyarc <nkemakolamsam@gmail.com>
2025-05-07 00:40:38 +0100

    Added routes for locations page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,12 @@ import Home from "./Pages/Home";
 import Blood from "./Pages/Blood";
 import Oxygen from "./Pages/Oxygen";
 import Stock from "./Pages/Stock";
-import ScrollToTop from "./components/Hooks/ScrollToTop";
+import ScrollToTop from "./Hooks/ScrollToTop";
 import Innovations from "./Pages/Innovations";
 import Careers from "./Pages/Careers";
 import Teams from "./Pages/Teams";
 import Contact from "./Pages/Contact";
+import Locations from "./Pages/Locations";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path="/careers" element={<Careers />} />
         <Route path="/team" element={<Teams />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/locations" element={<Locations />} />
       </Routes>
     </Router>
   );
