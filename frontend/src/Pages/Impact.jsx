296c8afc185d6c42f50d9b147c59fe84641a0bcd
Sammyarc <nkemakolamsam@gmail.com>
2025-05-08 03:27:13 +0100

    style modification

--- frontend/src/Pages/Impact.jsx
+++ frontend/src/Pages/Impact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/Navigation/Navbar";
 import CallToAction from "../components/CTAs/CallToAction";
 import Footer from "../components/Footer/Footer";
@@ -7,6 +7,19 @@ import ImpactGrid from "../components/Grids/ImpactGrid";
 import HealthcarePartnerships from "../components/CaseStudies/HealthcarePartnerships";
 
 const Impact = () => {
+  useEffect(() => {
+    const { hash } = window.location;
+    if (hash) {
+      // Delay to ensure the element is rendered
+      setTimeout(() => {
+        const el = document.querySelector(hash);
+        if (el) {
+          el.scrollIntoView({ behavior: "smooth" });
+        }
+      }, 100); // adjust delay as needed
+    }
+  }, []);
+
   return (
     <div>
       <nav>
